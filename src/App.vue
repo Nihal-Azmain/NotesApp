9fa7745b0e93e02f9603800595b3a8c733c39528
Nihal-Azmain <n.azmain01@gmail.com>
2023-01-31 16:07:28 +0600

    V 2.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <script setup>
-import { ref } from "vue"
+import { defineComponent, ref } from "vue"
+import overlayModule from "./overlayModule.vue"
+
 let id = 0;
 const overlay = ref(false);
-const newNote = ref("");
 const notes = ref([]);
 
 function getRandomColor() {
   return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
 }
 
-function addNote() {
-  if(newNote.value === "")
+const  addNote =(e,val) => {
+  
+  console.log(e,val);
+  if(val === "")
   {
     alert("Notes cannot be EMPTY!!!");
     return;
@@ -18,26 +21,20 @@ function addNote() {
 
   notes.value.push({
     id: ++id,
-    text: newNote.value,
+    text: val,
     date: new Date(),
     color: getRandomColor()
   })
   overlay.value=false;
-  newNote.value="";
 }
+
 
 </script>
 
 
 <template>
   <main>
-    <div class="overlay" v-show="overlay">
-      <div class="modal">
-        <textarea v-model="newNote" name="note" id="note" cols="30" rows="10"></textarea>
-        <button @click="addNote">Add Notes</button>
-        <button class="close" @click="overlay = false">Close</button>
-      </div>
-    </div>
+    <overlayModule :overlay="overlay" @close-modal="overlay=false" @send-text="addNote"/>
     <div class="container">
       <header>
         <h1>Notes {{ overlay }}</h1>
@@ -52,105 +49,6 @@ function addNote() {
         </div>
       </div>
     </div>
+    
   </main>
 </template>
-
-
-<style scoped>
-main {
-  height: 100vh;
-  width: 100vw;
-}
-
-.container {
-
-  max-width: 1000px;
-  padding: 10px;
-  margin: 0 auto;
-}
-
-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-h1 {
-  font-weight: bold;
-  margin-bottom: 25px;
-  font-size: 50px;
-}
-
-header button {
-  border: none;
-  padding: 10px;
-  width: 50px;
-  height: 50px;
-  cursor: pointer;
-  background-color: black;
-  border-radius: 100%;
-  color: white;
-  font-size: 25px;
-}
-
-.card {
-  width: 225px;
-  height: 225px;
-  background-color: yellow;
-  padding: 10px;
-  border-radius: 15px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  margin-right: 20px;
-  margin-bottom: 20px;
-
-}
-
-.cards-container {
-  display: flex;
-  flex-wrap: wrap;
-}
-
-.overlay {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.77);
-  z-index: 10;
-  display: flex;
-  justify-content: center;
-  padding-top: 200px;
-}
-
-.modal {
-  width: 750px;
-  height: 300px;
-  background-color: white;
-  border-radius: 10px;
-  padding: 30px;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-}
-
-.modal button {
-  padding: 10px 20px;
-  font-size: 20px;
-  width: 100%;
-  background-color: blueviolet;
-  border: none;
-  color: white;
-  cursor: pointer;
-  margin-top: 10px;
-}
-
-.modal .close {
-  background-color: red;
-  margin-top: 7px;
-}
-
-.date{
-  font-weight: bold;
-}
-</style>
---
+++ src/overlayModule.vue
@@ -0,0 +1,70 @@
+<script setup>
+    import { ref } from "vue";
+    const prop = defineProps(['overlay']);
+    const emit = defineEmits(['closeModal','sendText']);
+    const newNote =ref("");
+    const onNoteClick=(e)=>{
+      emit("sendText",e,newNote.value);
+      newNote.value="";
+    }
+    const onCloseClick=(e)=>{
+      emit("closeModal");
+      newNote.value="";
+    }
+</script>
+
+
+
+<template>
+    <div class="overlay" v-show="overlay">
+      <div class="modal">
+        <textarea v-model="newNote" name="note" id="note" cols="30" rows="10"></textarea>
+        <button @click="onNoteClick">Add Notes</button>
+        <button class="close" @click="onCloseClick">Close</button>
+      </div>
+    </div>
+</template>
+
+
+
+
+<style scoped>
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.77);
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  padding-top: 200px;
+}
+
+.modal {
+  width: 750px;
+  height: 300px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.modal button {
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: blueviolet;
+  border: none;
+  color: white;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.modal .close {
+  background-color: red;
+  margin-top: 7px;
+}
+
+</style>
